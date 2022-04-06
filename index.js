021d1872745599eae6fe4b2b90d2b5af8488f247
Mwai <machariamwai@gmail.com>
2022-04-06 12:10:40 +0300

    Add variables and perform arithmetic operations on them

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let num1 = 31;
+let num2 = 2;
+let multiply;
+multiply = num1 * num2;
+console.log(multiply)
 
-// Write your code here
+let random = Math.floor(Math.random()*10)+1;
+console.log(random)
+
+let mod, num3, num4;
+num3 = 16;
+num4 = 6;
+mod = num3%num4;
+console.log(mod)
+
+let max = Math.max(1,5,10,15,20)
+console.log(max)
